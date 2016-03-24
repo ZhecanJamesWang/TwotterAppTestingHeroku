6ee71c13918ac563a1917607ac41d18db2fa4a1a
tangtangwzc <tangtangwzc@gmail.com>
2016-03-23 21:04:07 -0400

    fixing

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ function ensureAuthenticated(req, res, next) {
 
 
 app.listen(process.env.PORT|| 3000, function() {
-  console.log("App running on port: 3000");
+  // console.log("App running on port: 3000");
 });
 
 module.exports = app;
